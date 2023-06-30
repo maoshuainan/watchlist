c962726621ad18e5cfdaa34025f821a96cd7b974
leomao <maoshuainan@outlook.com>
2023-06-30 19:21:58 +0800

    Create, edit and delete item by form

---
+++ config.py
@@ -0,0 +1,12 @@
+
+def dbconfig():
+    config = {
+        'prefix' : 'mysql+pymysql://',
+        'dbuser' : 'leomao',
+        'dbpwd' :'',
+        'localhost' : '127.0.0.1:3306',
+        'dbname' : 'watchlist',
+    }
+    return config
+
+
